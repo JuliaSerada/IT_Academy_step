b465b5abee381032f9833bc34fc7cadf797b966c
JuliaSerada <julia.serada@gmail.com>
2023-04-05 23:02:54 +0300

    Initial commit task3

---
+++ Stage12_Task3/model_logic.py
@@ -0,0 +1,5 @@
+def is_different(ls):
+    if len(ls) == len(set(ls)):
+        return True
+    else:
+        return False
---
+++ Stage12_Task3/task_main.py
@@ -0,0 +1,18 @@
+# The program that check is the randomly generated list (vector) with the different values
+
+import model_logic
+import value
+
+
+def main():
+    size = int(input("Input size of list: "))
+    ls = value.create_list(size)
+    value.rand_init(ls)
+    msg = value.convert(ls)
+    print("The list:", msg)
+    model_logic.is_different(ls)
+    print(f"This list is different: {model_logic.is_different(ls)}.")
+
+
+if __name__ == "__main__":
+    main()
